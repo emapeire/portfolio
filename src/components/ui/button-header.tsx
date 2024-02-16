e5fcebd3bc3171616f027ac7c3761d228ffd911f
Emanuel Peire <emapeire@outlook.com>
2024-02-16 16:40:15 -0300

    Update button-header styling

--- src/components/ui/button-header.tsx
+++ src/components/ui/button-header.tsx
@@ -7,7 +7,7 @@ export function ButtonHeader({
 }) {
   return (
     <a
-      className='border rounded-lg px-5 py-1.5 bg-white/10 border-white/10 shadow-lg shadow-white/10 hover:scale-110 transition-all ease-in-out hover:bg-white/20 hover:shadow-white/20'
+      className='border rounded-xl px-8 py-2 bg-white/5 border-white/15 shadow-xl shadow-white/5 hover:scale-110 transition-all ease-in-out hover:bg-white/10 hover:border-white/20 hover:shadow-white/10'
       href={href}
     >
       {children}
