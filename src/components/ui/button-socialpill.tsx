05159ee67d9ee2c2c6ee3e79374de3401b007385
Emanuel Peire <emapeire@outlook.com>
2024-02-15 23:24:25 -0300

    Add button-socialpill component

---
+++ src/components/ui/button-socialpill.tsx
@@ -0,0 +1,18 @@
+export function ButtonSocialPill({
+  href,
+  children
+}: {
+  href: string
+  children: React.ReactNode
+}) {
+  return (
+    <a
+      className='rounded-xl border border-white/5 shadow-md shadow-white/5 flex justify-center items-center gap-x-2 py-2 px-4 bg-white/5 hover:scale-105 hover:bg-white/10 transition md:my-4 my-3'
+      href={href}
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      {children}
+    </a>
+  )
+}
