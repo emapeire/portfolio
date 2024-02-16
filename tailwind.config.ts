21ab695ef56a7136743bcfd9091d700fc8941efc
Emanuel Peire <emapeire@outlook.com>
2024-02-15 21:24:47 -0300

    Update import statements and add font families

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,6 +71,10 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out'
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)']
       }
     }
   },
