8433565fe319f2ba360a050744c71074f0f67f07
Emanuel Peire <emapeire@outlook.com>
2024-02-16 16:22:53 -0300

    Reorder component imports and add header and footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import { About } from '@/components/about'
-import { Career } from '@/components/career'
-import { Navigation } from '@/components/navigation'
-import { Projects } from '@/components/projects'
+import { Header } from '@/components/header'
 import { Section } from '@/components/section'
-import { SectionSecondaryTitle } from '@/components/section-secondary-title'
-import { SectionTitleMain } from '@/components/section-title-main'
 import { Avatar } from '@/components/ui/avatar'
 import { Badge } from '@/components/ui/badge'
+import { SectionTitleMain } from '@/components/section-title-main'
+import { SectionSecondaryTitle } from '@/components/section-secondary-title'
+import { Navigation } from '@/components/navigation'
+import { About } from '@/components/about'
+import { Career } from '@/components/career'
+import { Projects } from '@/components/projects'
+import { Footer } from '@/components/footer'
 import { ABOUT as about } from '@/data'
 import { BriefCaseIcon } from '@/icons/brief-case'
 import { CodeIcon } from '@/icons/code'
@@ -15,6 +17,8 @@ import { ResumeIcon } from '@/icons/resume'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 px-6'>
+      <Header />
+
       <Section id='about' className='pb-32'>
         <Avatar />
 
@@ -55,6 +59,8 @@ export default function Home() {
         </SectionSecondaryTitle>
         <Projects />
       </Section>
+
+      <Footer />
     </main>
   )
 }
