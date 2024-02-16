2f4160c5c2976c4ba7098dd4e060a1a5aad3c581
Emanuel Peire <emapeire@outlook.com>
2024-02-16 18:02:14 -0300

    Refactor section title and badge styles

--- src/components/section-title-main.tsx
+++ src/components/section-title-main.tsx
@@ -2,16 +2,18 @@ import { Badge } from './ui/badge'
 
 export function SectionTitleMain() {
   return (
-    <h1 className='text-gray-100 text-4xl md:text-5xl font-bold flex flex-row gap-x-4 pb-12 text-balance items-center'>
-      Hey, I&apos;m Emanuel Peire
+    <div className='flex flex-col md:flex-row md:gap-5 gap-2 pb-12'>
+      <h1 className='flex items-center text-gray-100 text-4xl md:text-5xl font-bold text-balance'>
+        Hey, I&apos;m Emanuel Peire
+      </h1>
       <a
         href='https://linkedin.com/in/emanuelpeire'
         target='_blank'
         rel='noopener noreferrer'
-        className='flex justify-center items-center'
+        className='flex items-center mr-auto ml-0 hover:scale-105 transition-all ease-in-out'
       >
         <Badge>Available for hire</Badge>
       </a>
-    </h1>
+    </div>
   )
 }
