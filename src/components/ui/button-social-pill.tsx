0f427b103c715ee77d57a854390d0ad556a8becf
Emanuel Peire <emapeire@outlook.com>
2024-02-19 12:17:12 -0300

    Update button-social-pill styling

--- src/components/ui/button-social-pill.tsx
+++ src/components/ui/button-social-pill.tsx
@@ -7,7 +7,7 @@ export function ButtonSocialPill({
 }) {
   return (
     <a
-      className='rounded-xl border border-white/5 shadow-md shadow-white/5 flex md:justify-center justify-start items-center md:gap-x-2 gap-x-16 py-3 md:py-2 md:px-4 px-12 bg-white/5 hover:scale-105 hover:bg-white/10 transition md:my-4 my-3'
+      className='rounded-xl border border-white/5 shadow-md shadow-white/5 flex md:justify-center justify-start items-center md:gap-x-2 gap-x-8 py-3 md:py-2 md:px-4 px-8 bg-white/5 hover:scale-105 hover:bg-white/10 transition md:my-4 my-3'
       href={href}
       target='_blank'
       rel='noopener noreferrer'
