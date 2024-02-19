c90c9acc4d07ed1ef2b86e6e46aab1a7356d42f5
Emanuel Peire <emapeire@outlook.com>
2024-02-19 11:39:41 -0300

    Update footer and header styles

--- src/components/ui/button-header.tsx
+++ src/components/ui/button-header.tsx
@@ -7,7 +7,7 @@ export function ButtonHeader({
 }) {
   return (
     <a
-      className='border rounded-xl px-6 md:px-12 py-2.5 bg-white/5 border-white/10 shadow-xl shadow-white/5 hover:scale-110 transition-all ease-in-out hover:bg-white/10'
+      className='rounded-[11px] px-6 md:px-12 py-3 transition-all ease-in-out duration-300 hover:bg-white/10'
       href={href}
     >
       {children}
