a99152dc0781a66f9ab612f3d8f63541fe9d8213
Emanuel Peire <emapeire@outlook.com>
2024-02-20 02:25:53 -0300

    Refactor Projects component to improve readability and structure

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -7,59 +7,66 @@ import { BadgeTag } from './ui/badge-tag'
 export function Projects() {
   return (
     <div className='flex flex-col gap-16'>
-      {projects.map(({ title, description, tags, image, video, link }) => (
-        <article key={link}>
-          <a href={link} target='_blank' rel='noopener noreferrer'>
-            <h3 className='flex items-center gap-x-2 text-xl md:text-2xl font-semibold mb-4 ml-1.5'>
-              <LinkIcon className='size-6' />
-              <span className='text-blue-100 hover:underline'>{title}</span>
-            </h3>
-          </a>
-          <div className='ml-10'>
-            <p className='text-base md:text-lg mb-4 text-pretty'>
-              {description}
-            </p>
-            <div
-              className={clsx('flex flex-col', {
-                'gap-6': image ?? video
-              })}
-            >
-              <ul className='md:flex grid grid-cols-2 md:gap-x-4 gap-x-0 gap-4 items-center justify-items-start'>
-                {tags.map((t) => (
-                  <li key={t.name}>
-                    <BadgeTag>
-                      <t.icon className='size-4' />
-                      {t.name}
-                    </BadgeTag>
-                  </li>
-                ))}
-              </ul>
-              {image && (
-                <Image
-                  className='rounded-xl shadow-2xl shadow-white/5 border border-white/10'
-                  width={800}
-                  height={400}
-                  src={image}
-                  alt={title}
-                />
-              )}
-              {video && (
-                <video
-                  className='rounded-xl shadow-2xl shadow-white/5 border border-white/10'
-                  width={800}
-                  height={400}
-                  controls
-                  muted
-                  autoPlay
-                  loop
+      {projects.map(
+        ({ title, description, tags, image, video, link }, index) => (
+          <>
+            <article key={link}>
+              <a href={link} target='_blank' rel='noopener noreferrer'>
+                <h3 className='flex items-center gap-x-2 text-xl md:text-2xl font-semibold mb-4 ml-1.5'>
+                  <LinkIcon className='size-6' />
+                  <span className='text-blue-100 hover:underline'>{title}</span>
+                </h3>
+              </a>
+              <div className='ml-10'>
+                <p className='text-base md:text-lg mb-4 text-pretty'>
+                  {description}
+                </p>
+                <div
+                  className={clsx('flex flex-col', {
+                    'gap-6': image ?? video
+                  })}
                 >
-                  <source src={video} type='video/webm' />
-                </video>
-              )}
-            </div>
-          </div>
-        </article>
-      ))}
+                  <ul className='md:flex grid grid-cols-2 md:gap-x-4 gap-x-0 gap-4 items-center justify-items-start'>
+                    {tags.map((t) => (
+                      <li key={t.name}>
+                        <BadgeTag>
+                          <t.icon className='size-4' />
+                          {t.name}
+                        </BadgeTag>
+                      </li>
+                    ))}
+                  </ul>
+                  {image && (
+                    <Image
+                      className='rounded-xl shadow-2xl shadow-white/5 border border-white/10'
+                      width={800}
+                      height={400}
+                      src={image}
+                      alt={title}
+                    />
+                  )}
+                  {video && (
+                    <video
+                      className='rounded-xl shadow-2xl shadow-white/5 border border-white/10'
+                      width={800}
+                      height={400}
+                      controls
+                      muted
+                      autoPlay
+                      loop
+                    >
+                      <source src={video} type='video/webm' />
+                    </video>
+                  )}
+                </div>
+              </div>
+            </article>
+            {index < projects.length - 1 && (
+              <div className='border-b ml-2 border-white/20' />
+            )}
+          </>
+        )
+      )}
     </div>
   )
 }
