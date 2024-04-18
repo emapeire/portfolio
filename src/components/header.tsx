739225e092cdf9716c8841bdef495b552ae507ea
Emanuel Peire <emapeire@outlook.com>
2024-04-18 16:05:36 -0300

    Update header component with new navigation items and mode toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,44 @@
-import { ButtonHeader } from './ui/button-header'
+import { ModeToggle } from './mode-toggle'
 
 export function Header() {
+  const navItems = [
+    {
+      title: 'About',
+      label: 'about',
+      url: '/#about'
+    },
+    {
+      title: 'Career',
+      label: 'career',
+      url: '/#career'
+    },
+    {
+      title: 'Projects',
+      label: 'projects',
+      url: '/#projects'
+    },
+    {
+      title: 'Contact',
+      label: 'contact',
+      url: 'mailto:me@emapeire.xyz'
+    }
+  ]
+
   return (
-    <header className='flex justify-center font-medium text-base items-center pt-16 pb-32 md:pb-40 w-full mx-auto'>
-      <nav className='flex flex-row text-neutral-100 items-center justify-center text-center border bg-neutral-800/30 shadow rounded-xl'>
-        <ButtonHeader href='#about'>About</ButtonHeader>
-        <ButtonHeader href='#career'>Career</ButtonHeader>
-        <ButtonHeader href='#projects'>Projects</ButtonHeader>
+    <header className='fixed top-0 z-10 flex items-center justify-center w-full mx-auto mt-4 gap-x-8'>
+      <nav className='flex w-full max-w-2xl border text-sm font-semibold rounded-md text-neutral-800 dark:text-neutral-100 justify-center items-center'>
+        {navItems.map((link) => (
+          <a
+            key={link.label}
+            aria-label={link.label}
+            href={link.url}
+            className='relative block mx-4 my-2 transition-colors ease-in-out hover:text-neutral-600 dark:hover:text-neutral-300'
+          >
+            {link.title}
+          </a>
+        ))}
       </nav>
+      <ModeToggle />
     </header>
   )
 }
