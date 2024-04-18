dcdd4a1b53b10ffbda74d8520dff2e6b435f9839
Emanuel Peire <emapeire@outlook.com>
2024-04-18 18:17:30 -0300

    Refactor styles and navigation in globals.css and header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -25,19 +25,19 @@ export function Header() {
   ]
 
   return (
-    <header className='fixed top-0 z-10 flex items-center justify-center w-full mx-auto mt-4'>
-      <nav className='nav-header flex px-4 py-1 md:px-48 text-sm font-semibold rounded-md text-neutral-800 dark:text-neutral-100 justify-center items-center'>
+    <header className='fixed top-0 z-10 flex items-center justify-center w-full mx-auto bg-white dark:bg-black border-b py-2'>
+      <nav className='flex px-4 py-1 md:px-48 justify-center items-center'>
         {navItems.map((link) => (
           <a
             key={link.label}
             aria-label={link.label}
             href={link.url}
-            className='relative block mx-1 transition-colors ease-in-out py-2 px-4 rounded-sm hover:dark:bg-neutral-800 hover:bg-neutral-100'
+            className='relative block mx-1 transition-colors ease-in-out py-2 px-4 rounded-sm text-sm font-semibold text-neutral-800 dark:text-neutral-100 hover:dark:bg-neutral-800 hover:bg-neutral-100'
           >
             {link.title}
           </a>
         ))}
-        <div className='flex items-center ms-1'>
+        <div className='flex items-center ms-2'>
           <ModeToggle />
         </div>
       </nav>
