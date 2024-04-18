59eda24d96a042d6fea942a822f0e299754d3e68
Emanuel Peire <emapeire@outlook.com>
2024-04-18 16:59:57 -0300

    Update header component with new navigation items and mode toggle

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -17,7 +17,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='link' size='icon'>
+        <Button variant='link' size='icon' className='size-6'>
           <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 hover:text-neutral-600' />
           <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 hover:text-neutral-300' />
           <span className='sr-only'>Toggle theme</span>
