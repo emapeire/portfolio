1999ff5339fe18b82b2821c83dc5bc3835e4af7c
Emanuel Peire <emapeire@outlook.com>
2024-04-19 22:11:53 -0300

    Refactor hero component styles and navigation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -34,7 +34,7 @@ export function Hero() {
           href='https://linkedin.com/in/emanuelpeire'
           target='_blank'
           rel='noopener noreferrer'
-          className='hidden md:block'
+          className='hidden md:flex'
         >
           <Badge
             variant='default'
@@ -47,29 +47,29 @@ export function Hero() {
           href='https://linkedin.com/in/emanuelpeire'
           target='_blank'
           rel='noopener noreferrer'
-          className='md:hidden block hover:scale-105 transition-all ease-in-out duration-300'
+          className='md:hidden flex hover:scale-105 transition-all ease-in-out duration-300'
         >
-          <BadgeCheckIcon className='size-8 dark:fill-blue-400 fill-blue-300' />
+          <BadgeCheckIcon className='size-8 dark:fill-blue-400 fill-blue-200' />
         </a>
       </div>
 
       <div className='flex flex-col font-mono gap-4 dark:text-neutral-200 text-neutral-800 text-pretty'>
-        <h2>{about.title}</h2>
+        <h3>{about.title}</h3>
         <h3>{about.description}</h3>
       </div>
 
       <nav className='flex gap-x-4'>
         {links.map((link) => (
-          <Button variant='secondary' key={link.title} asChild>
+          <Button variant='secondary' key={link.title} className='px-2' asChild>
             <a
               key={link.title}
               href={link.url}
               target='_blank'
               rel='noopener noreferrer'
-              className='flex justify-center items-center border'
+              className='flex justify-center items-center md:gap-2'
             >
               <link.icon className='size-6' />
-              <p>{link.title}</p>
+              <p className='hidden md:block'>{link.title}</p>
             </a>
           </Button>
         ))}
