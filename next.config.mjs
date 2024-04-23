74b1eb72b1ed3e0842712bee854cad1e46b05a2c
Emanuel Peire <emapeire@outlook.com>
2024-04-22 21:40:17 -0300

    Refactor project card styles and structure in projects.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/_vercel/:path*/script.js',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable'
-          }
-        ]
-      }
-    ]
-  },
   images: {
     formats: ['image/avif', 'image/webp']
   }
