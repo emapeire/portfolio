6f5d79449d98c50f5493e60c1dcc940334c649c3
Emanuel Peire <emapeire@outlook.com>
2024-05-07 16:19:49 -0300

    chore: Update header component to improve navigation layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,28 +11,30 @@ export function Header() {
 
   return (
     <header className='fixed top-0 z-10 flex items-center justify-center w-full mx-auto bg-white dark:bg-black border-b py-2'>
-      <nav className='flex justify-center items-center'>
-        {links.map((link) => (
-          <Link
-            key={link.label}
-            aria-label={link.label}
-            href={link.url}
-            className={clsx(
-              'relative block mx-1 transition-colors ease-in-out py-2 md:px-4 px-2 rounded-md text-sm font-semibold text-neutral-800 dark:text-neutral-100',
-              {
-                'bg-neutral-100 dark:bg-neutral-800': pathname === link.url,
-                'hover:bg-neutral-100 dark:hover:bg-neutral-800':
-                  pathname !== link.url
-              }
-            )}
-          >
-            {link.title}
-          </Link>
-        ))}
-        <div className='flex items-center ms-1'>
+      <div className='flex items-center justify-between w-full md:max-w-3xl mx-4'>
+        <nav className='flex justify-center items-center'>
+          {links.map((link) => (
+            <Link
+              key={link.label}
+              aria-label={link.label}
+              href={link.url}
+              className={clsx(
+                'relative block me-2 transition-colors ease-in-out py-2 md:px-4 px-2 rounded-md text-sm font-semibold text-neutral-800 dark:text-neutral-100',
+                {
+                  'bg-neutral-100 dark:bg-neutral-800': pathname === link.url,
+                  'hover:bg-neutral-100 dark:hover:bg-neutral-800':
+                    pathname !== link.url
+                }
+              )}
+            >
+              {link.title}
+            </Link>
+          ))}
+        </nav>
+        <div className='flex items-center'>
           <ModeToggle />
         </div>
-      </nav>
+      </div>
     </header>
   )
 }
