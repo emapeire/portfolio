af4cb693aae72eda4ebb771e7b061a11b69bde8c
Emanuel Peire <emapeire@outlook.com>
2024-05-08 14:23:02 -0300

    Refactor SVG icon components to use arrow function syntax

--- src/components/icons/tags/shadcn-ui.tsx
+++ src/components/icons/tags/shadcn-ui.tsx
@@ -1,20 +1,18 @@
 import type { SVGProps } from 'react'
 
-export const ShadcnIcon = (props: SVGProps<SVGSVGElement>) => {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 256 256'
-      className={`rounded-[2px] ${props.className ?? ''}`}
-    >
-      <path fill='#000' d='M0 0h256v256H0z'></path>
-      <path
-        fill='none'
-        stroke='#FFFFFF'
-        strokeWidth='8'
-        d='M208 128l-80 80M192 40L40 192'
-      ></path>
-    </svg>
-  )
-}
+export const ShadcnIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 256 256'
+    className={`rounded-[2px] ${props.className ?? ''}`}
+  >
+    <path fill='#000' d='M0 0h256v256H0z'></path>
+    <path
+      fill='none'
+      stroke='#FFFFFF'
+      strokeWidth='8'
+      d='M208 128l-80 80M192 40L40 192'
+    ></path>
+  </svg>
+)
