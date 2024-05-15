83597f1b0fcf6ccfd232a9a44fa7cbd5a8b81ad2
Emanuel Peire <emapeire@outlook.com>
2024-05-15 14:11:20 -0300

    refactor: Update import statement for next/link in pagination component

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -3,7 +3,7 @@ import { ChevronLeft, ChevronRight, MoreHorizontal } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 import { type ButtonProps, buttonVariants } from '@/components/ui/button'
-import { Link } from 'next-view-transitions'
+import Link from 'next/link'
 
 const Pagination = ({ className, ...props }: React.ComponentProps<'nav'>) => (
   <nav
