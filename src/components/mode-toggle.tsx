7b8d6c629e79ede0161ecc52a58e9b4125ab67df
Emanuel Peire <emapeire@outlook.com>
2024-08-21 17:31:41 -0300

    fix padding in settings

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -23,14 +23,16 @@ export function ModeToggle() {
           variant='ghost'
           aria-label={t('mode-toggle.label')}
           size='sm'
-          className='gap-2'
+          className='w-full mx-auto flex justify-between'
         >
           <DropdownMenuLabel className='p-0'>
             {t('mode-toggle.label')}
           </DropdownMenuLabel>
 
-          <Sun className='h-[1.5rem] w-[1.5rem] stroke-[1.5] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <Moon className='h-[1.5rem] w-[1.5rem] stroke-[1.5] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          <div className='relative h-[1.5rem] w-[1.5rem]'>
+            <Sun className='absolute h-full w-full stroke-[1.5] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+            <Moon className='absolute h-full w-full stroke-[1.5] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          </div>
 
           <span className='sr-only'>{t('mode-toggle.label')}</span>
         </Button>
