caf9c60297cca4d699287c8e744201c5cc267e9e
Emanuel Peire <emapeire@outlook.com>
2024-08-27 00:26:24 -0300

    Add projects translations

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -4,6 +4,7 @@ import { usePagination } from '@/hook/use-pagination'
 import { Button } from '../ui/button'
 import { ProjectCard } from './card'
 import { ProjectPagination } from './pagination'
+import { useTranslations } from 'next-intl'
 import { TerminalIcon } from 'lucide-react'
 import { data } from '@/constants'
 
@@ -12,18 +13,19 @@ export function Projects() {
   const { currentProjects, page, totalPages, updatePage } = usePagination({
     projects
   })
+  const t = useTranslations()
 
   return (
     <>
       {currentProjects.length === 0 || page < 1 || page > totalPages ? (
         <Button variant='secondary' size='sm' asChild>
-          <a href='/projects'>No projects found</a>
+          <a href='/projects'>{t('projects.not-found')}</a>
         </Button>
       ) : (
         <>
           <h2 className='dark:text-neutral-100 text-neutral-800 flex gap-x-4 items-center text-3xl font-bold text-balance'>
             <TerminalIcon className='size-6 animate-pulse stroke-[1.5]' />
-            Projects & Crafts
+            {t('projects.title')}
           </h2>
 
           <ProjectCard projects={currentProjects} />
